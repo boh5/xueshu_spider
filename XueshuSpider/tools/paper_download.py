984e5013d7f78741dacfe9f897a15e4a0feea411
dill <bohuang1994@gmail.com>
2018-07-04 22:41:31 +0800

    add error log to download module

--- XueshuSpider/tools/paper_download.py
+++ XueshuSpider/tools/paper_download.py
@@ -4,10 +4,13 @@
 @Time    : 2018/6/25 22:46
 @File    : paper_download.py
 """
+import os
 import re
+from datetime import datetime
 
 import MySQLdb
 from selenium import webdriver
+from selenium.common.exceptions import NoSuchElementException
 
 
 class MySQLAccess(object):
@@ -57,18 +60,27 @@ class DownloadPaper():
 
     def download_paper(self, url):
         self.browser.get(url)
-        if re.match(r'^(http://kns.cnki.net).*', url):
-            # 知网期刊
-            self.browser.find_element_by_css_selector('#pdfDown').click()
-        if re.match(r'^(http://http://cdmd.cnki.com.cn).*', url):
-            # 知网空间
-            self.browser.find_element_by_css_selector('#ty_caj > a:nth-child(1)').click()
-        if re.match(r'^(http://cpfd.cnki.com.cn).*', url):
-            # cpfd知网
-            self.browser.find_element_by_css_selector('#ty_pdf > a:nth-child(1)').click()
-        if re.match(r'^(http://www.wanfangdata.com.cn).*', url):
-            # 万方
-            self.browser.find_element_by_css_selector('#ddownb').click()
+        try:
+            if re.match(r'^(http://kns.cnki.net).*', url):
+                # 知网期刊
+                self.browser.find_element_by_css_selector('#pdfDown').click()
+            if re.match(r'^(http://http://cdmd.cnki.com.cn).*', url):
+                # 知网空间
+                self.browser.find_element_by_css_selector('#ty_caj > a:nth-child(1)').click()
+            if re.match(r'^(http://cpfd.cnki.com.cn).*', url):
+                # cpfd知网
+                self.browser.find_element_by_css_selector('#ty_pdf > a:nth-child(1)').click()
+            if re.match(r'^(http://www.wanfangdata.com.cn).*', url):
+                # 万方
+                self.browser.find_element_by_css_selector('#ddownb').click()
+        except NoSuchElementException as e:
+            current_path = os.path.dirname(__file__)
+            project_path = os.path.join(current_path, '../..')
+            down_path = os.path.abspath(os.path.join(project_path, 'down_papers'))
+            file_path = os.path.join(down_path, 'down_error_log.txt')
+            with open(file_path, 'w+', encoding='utf-8') as error_file:
+                error_file.write(str(datetime.now()) + ': ' + url + " -- don't provide download.")
+                error_file.write('\r\n')
 
 
 if __name__ == '__main__':
